192f860110008046d616e3a2652af5118ccfb12c
Ferdian Ifkarsyah <ferdian.ifkarsyah@gmail.com>
2020-02-07 11:37:09 +0700

    change typo Comment to URL

--- app/models/URL.scala
+++ app/models/URL.scala
@@ -5,5 +5,5 @@ import play.api.libs.json.{Json, OFormat}
 case class URL(shortURL: String, originalURL: String)
 
 object URL {
-  implicit val format: OFormat[Comment] = Json.format[Comment]
+  implicit val format: OFormat[URL] = Json.format[URL]
 }
